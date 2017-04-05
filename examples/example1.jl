c2639852b436f6769aef39776e3b96831f45b951
Stephen Nicar <stephenbnicar@gmail.com>
2017-04-04 20:43:33 -0400

    Update example

--- examples/example1.jl
+++ examples/example1.jl
@@ -4,7 +4,7 @@ using DataFrames
 # Example from Lutkepohl (2005), Section 3.2.3, p.77
 e1data = readtable("e1data.csv")
 e1data[:date] = Date(e1data[:date])
-e1data2 = e1data[Date(1960,12,1).<e1data[:date].< Date(1979,3,1), :]
+e1data2 = e1data[e1data[:date].< Date(1979,3,1), :]
 
 datamat  = diff(log(Matrix(e1data2[:, 2:end])))
 constant = true
